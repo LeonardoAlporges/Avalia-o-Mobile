9a11309c5f1418bfd1911c26218185984c26c24f
leonardo <leonardofixaa@gmail.com>
2020-01-29 15:44:53 -0200

    teste

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,35 +12,59 @@ export class DataService implements InMemoryDbService{
 
    let  aniversariante = 
    [
-     {"dia":10, "mes":1,"nome":"leonardo Alporges Martins"},
-     {"dia":1, "mes":2,"nome":"José da Silva Teixeira"},
-     {"dia":12, "mes":3,"nome":"Luiz  Carlos Garcia"},
-     {"dia":15, "mes":4,"nome":"Maria Aparecida Vieira"},
-     {"dia":53, "mes":5,"nome":"Josefa Elisa Ribeiro"},
-     {"dia":24, "mes":6,"nome":"Mário Vicente Vinicius Assis"},
-     {"dia":25, "mes":7,"nome":"Tânia Manuela Sophia da Rosa"},
-     {"dia":26, "mes":8,"nome":"Raquel Alessandra Rebeca Assis"},
-     {"dia":27, "mes":9,"nome":"Thiago Enzo da Rosa"},
-     {"dia":28, "mes":29,"nome":"Rayssa Antonella Juliana Barros"},
-     {"dia":30, "mes":11,"nome":"Patrícia Amanda Raimunda da Luz"},
-     {"dia":31, "mes":12,"nome":"Mateus Benedito Thomas Galvão"},
-     {"dia":1, "mes":1,"nome":"Ryan Cauã Paulo da Silva"},
-     {"dia":2, "mes":2,"nome":"Elza Carla Moura"},
-     {"dia":3, "mes":3,"nome":"Carlos Eduardo Guilherme Barbosa"},
-     {"dia":4, "mes":4,"nome":"Valentina Amanda Márcia Caldeira"},
-     {"dia":5, "mes":5,"nome":"Gabriela Catarina Andrea Assunção"},
-     {"dia":6, "mes":6,"nome":"Lívia Nina Mariana Lima"},
-     {"dia":7, "mes":7,"nome":"Yuri Felipe Duarte"},
-     {"dia":8, "mes":8,"nome":"Samuel Bruno da Costa"},
-     {"dia":9, "mes":9,"nome":"Julio Matheus Baptista"},
-     {"dia":11, "mes":12,"nome":"Rayssa Antonella Juliana Barros"},
-     {"dia":13, "mes":11,"nome":"Patrícia Amanda Raimunda da Luz"},
-     {"dia":14, "mes":12,"nome":"Mateus Benedito Thomas Galvão"},
-     {"dia":15, "mes":1,"nome":"leonardo Alporges Martins"},
-     {"dia":16, "mes":2,"nome":"José da Silva Teixeira"},
-     {"dia":17, "mes":3,"nome":"Luiz  Carlos Garcia"},
+    {"dia":10, "mes":1,"nome":"leonardo Alporges Martins"},
+    {"dia":10, "mes":1,"nome":"José da Silva Teixeira"},
+    {"dia":10, "mes":1,"nome":"Luiz  Carlos Garcia"},
+    {"dia":15, "mes":1,"nome":"Maria Aparecida Vieira"},
+    {"dia":15, "mes":1,"nome":"Josefa Elisa Ribeiro"},
+    {"dia":24, "mes":1,"nome":"Mário Vicente Vinicius Assis"},
+    {"dia":31, "mes":1,"nome":"leonardo Alporges Martins"},
+    {"dia":31, "mes":1,"nome":"José da Silva Teixeira"},
+    {"dia":31, "mes":1,"nome":"Luiz  Carlos Garcia"},
+    {"dia":15, "mes":1,"nome":"Maria Aparecida Vieira"},
+    {"dia":15, "mes":1,"nome":"Josefa Elisa Ribeiro"},
+    {"dia":24, "mes":1,"nome":"Mário Vicente Vinicius Assis"},
+    {"dia":24, "mes":2,"nome":"Mário Vicente Vinicius Assis"},
+    {"dia":24, "mes":2,"nome":"Mário Vicente Vinicius Assis"},
+    {"dia":24, "mes":2,"nome":"Mário Vicente Vinicius Assis"},
+   
+  ]
+   return {aniversariante};
+
+  }
+}
+
+
+/*
+  {"dia":10, "mes":1,"nome":"leonardo Alporges Martins"},
+     {"dia":10, "mes":1,"nome":"José da Silva Teixeira"},
+     {"dia":10, "mes":1,"nome":"Luiz  Carlos Garcia"},
+     {"dia":15, "mes":1,"nome":"Maria Aparecida Vieira"},
+     {"dia":15, "mes":1,"nome":"Josefa Elisa Ribeiro"},
+     {"dia":24, "mes":1,"nome":"Mário Vicente Vinicius Assis"},
+     {"dia":10, "mes":2,"nome":"Tânia Manuela Sophia da Rosa"},
+     {"dia":10, "mes":2,"nome":"Raquel Alessandra Rebeca Assis"},
+     {"dia":10, "mes":2,"nome":"Thiago Enzo da Rosa"},
+     {"dia":15, "mes":2,"nome":"Rayssa Antonella Juliana Barros"},
+     {"dia":15, "mes":2,"nome":"Patrícia Amanda Raimunda da Luz"},
+     {"dia":24, "mes":2,"nome":"Mateus Benedito Thomas Galvão"},
+     {"dia":31, "mes":2,"nome":"Ryan Cauã Paulo da Silva"},
+     {"dia":20, "mes":2,"nome":"Elza Carla Moura"},
+     {"dia":30, "mes":3,"nome":"Carlos Eduardo Guilherme Barbosa"},
+     {"dia":23, "mes":3,"nome":"Valentina Amanda Márcia Caldeira"},
+     {"dia":5, "mes":3,"nome":"Gabriela Catarina Andrea Assunção"},
+     {"dia":6, "mes":3,"nome":"Lívia Nina Mariana Lima"},
+     {"dia":7, "mes":3,"nome":"Yuri Felipe Duarte"},
+     {"dia":8, "mes":3,"nome":"Samuel Bruno da Costa"},
+     {"dia":9, "mes":3,"nome":"Julio Matheus Baptista"},
+     {"dia":11, "mes":3,"nome":"Rayssa Antonella Juliana Barros"},
+     {"dia":13, "mes":4,"nome":"Patrícia Amanda Raimunda da Luz"},
+     {"dia":14, "mes":4,"nome":"Mateus Benedito Thomas Galvão"},
+     {"dia":15, "mes":4,"nome":"leonardo Alporges Martins"},
+     {"dia":16, "mes":4,"nome":"José da Silva Teixeira"},
+     {"dia":17, "mes":4,"nome":"Luiz  Carlos Garcia"},
      {"dia":18, "mes":4,"nome":"Maria Aparecida Vieira"},
-     {"dia":19, "mes":5,"nome":"Josefa Elisa Ribeiro"},
+     {"dia":19, "mes":4,"nome":"Josefa Elisa Ribeiro"},
      {"dia":20, "mes":6,"nome":"Mário Vicente Vinicius Assis"},
      {"dia":21, "mes":7,"nome":"Tânia Manuela Sophia da Rosa"},
      {"dia":22, "mes":8,"nome":"Raquel Alessandra Rebeca Assis"},
@@ -105,9 +129,4 @@ export class DataService implements InMemoryDbService{
      {"dia":22, "mes":7,"nome":"Yuri Felipe Duarte"},
      {"dia":10, "mes":8,"nome":"Samuel Bruno da Costa"},
      
-   
-  ]
-   return {aniversariante};
-
-  }
-}
+*/
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,35 +14,52 @@ export class HomePage {
   Aniversarios: any[] = [];
   mes : string;
   nome: string;
-  isHidden: boolean = true;
-  contador: number = 0;
   dia: number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,21,22,23,24,25,26,27,29,29,30];
+
+  MesmoDia: any[] = [];
+
 
   constructor(private CalendarioService: CalendarioService) { }
 
   ngOnInit() {
-    
+    this.CalendarioService.getAniversariantes().subscribe((data : any[])=>{
+    this.Aniversarios = data;
+    console.log(this.Aniversarios);
+  });
   }
 
-  BuscarData(){
-    this.CalendarioService.getAniversariantes().subscribe((data : any[])=>{
-      this.Aniversarios = data;
-      console.log(this.Aniversarios);
-  })
+  teste(test){
+    var position = 0;
+    var j = 0;
+
+    for (var i=0; i <= this.Aniversarios.length; i++)
+    {
+      var controlador = this.Aniversarios[i].dia; 
+      if( controlador == test.dia)
+      {
+          this.MesmoDia[j] =this.Aniversarios[i] ;
+          position++;
+          j++;
+      }
+    }
   }
-  
+
+
+
+
 
   enviarValor(event){
     var controlador = event.detail.value;
     this.mes = controlador.substr(6,7);
     this.mes = this.mes.substr(0,1);
-    this.BuscarData(); 
   }
 
   enviarNome(event){
     this.nome = event.detail.value;
-    this.BuscarData();
-  }
 
+  }
+  semnome(test){
+
+  }
 
 }
